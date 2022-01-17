a217e1f89ee23233378403628615313438a5edbe
AsilbekBoboqulov <93362474+AsilbekBoboqulov@users.noreply.github.com>
2022-01-17 22:27:16 +0500

    Create main_25.cpp
    same operation in cpp

---
+++ main_25.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "hello\n";
+    cout << "hello" << endl;
+    cout << "\'hello" << endl;
+    cout << "\"hello" << endl;
+    cout << "\?hello" << endl;
+    cout << "\ahello" << endl;
+    cout << "hello1" << "\xxx" << endl;
+    cout << "hello world\r" << endl;
+    cout << "hel"<<'t'<<"lo"<< endl;
+    cout << "he"<<'\b'<<"ello"<< endl;
+    return 0;
+}
