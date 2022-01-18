00886361d1c0f8999909edbf17c2131086412a75
AsilbekBoboqulov <93362474+AsilbekBoboqulov@users.noreply.github.com>
2022-01-18 15:32:29 +0500

    Create main_26.cpp
    absolute rooms

---
+++ main_26.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    long double a = 2,b=3,c; int p,q;
+    cin >> q;
+    p=cout.precision(q);
+    cout<<p<<" room"<<endl;
+    c=a/b;
+    cout<<"c= "<<c<<endl;
+    return 0;
+}
