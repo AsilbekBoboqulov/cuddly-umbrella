4b04fee8aa79cfd4e653d1d5e22c9c499d4432c9
AsilbekBoboqulov <93362474+AsilbekBoboqulov@users.noreply.github.com>
2022-01-18 20:38:59 +0500

    Create main_33.cpp
    increment and decrement

---
+++ main_33.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 10;
+    cout << a-- << endl;
+    cout << a << endl;
+
+    int b = 20;
+    cout << b++ << endl;
+    cout << b << endl;
+
+    int c = 30;
+    cout << ++c << endl;
+    cout << c++ << endl;
+
+    int d = 40;
+    cout << --d << endl;
+    cout << d-- << endl;
+}
