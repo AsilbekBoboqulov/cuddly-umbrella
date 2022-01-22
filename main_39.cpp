75da17385e88c302a7fcfd0a5da18a98122288e4
AsilbekBoboqulov <93362474+AsilbekBoboqulov@users.noreply.github.com>
2022-01-22 14:17:25 +0500

    Create main_39.cpp
    double = 64 bit = 8 byte
    int        = 16 bit = 2 byte
    float     = 32 bit = 4 byte

---
+++ main_39.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    double a = 1, b = 1.2;
+    cout << a + b << endl << endl;
+    int q = 1, w = 1.2;
+    cout << q + w << endl << endl;
+    float e = 1, r = 1.2;
+    cout << e + r;
+
+    return 0;
+}
