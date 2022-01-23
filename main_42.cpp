709660596169878fd4ad72f1b2ec11c9607055c4
AsilbekBoboqulov <93362474+AsilbekBoboqulov@users.noreply.github.com>
2022-01-23 20:09:54 +0500

    Create main_42.cpp
    if else operators

---
+++ main_42.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    bool number = true;
+    if(number){
+        cout << "num";
+    } else{
+        cout << "NAN";
+    }
+    return 0;
+}
