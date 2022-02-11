d1f0e9b3b1a9acc360fcb6440dacca013ca8f181
AsilbekBoboqulov <93362474+AsilbekBoboqulov@users.noreply.github.com>
2022-02-11 22:45:36 +0500

    Create main_66.cpp
    rounding

---
+++ main_66.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	float a = 8.9, b = 7.1;
+	cout << "rounding down " << floor(a) << " " << floor(b) << "\n";
+	cout << "rounding up " << ceil(a) << " " << ceil(b);
+}
