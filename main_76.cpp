519ac902740767072ab35f14be429e09f4b5b3dd
AsilbekBoboqulov <93362474+AsilbekBoboqulov@users.noreply.github.com>
2022-03-28 12:47:08 +0500

    Create main_76.cpp
    pow limit

---
+++ main_76.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+int main()
+{
+	unsigned long long a,b; scanf("%d%d",&a,&b);
+	printf("%.f",pow(a,b));
+}
+//2 1023
+//4 511
+//8 341
+//16 255
+//32 204
+//64 170
+//128 146
+//256 127
+//512 112
+//1024 102
