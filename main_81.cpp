92b9e40cea016c611e27261aad597e9155de364c
AsilbekBoboqulov <93362474+AsilbekBoboqulov@users.noreply.github.com>
2022-04-04 23:11:40 +0500

    Create main_81.cpp
    super strings method

---
+++ main_81.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+int main(){
+	/*
+	string s1 = "Hello World!";
+	s1.clear();
+	cout<<s1;
+	*/
+	
+	/*
+	string s1 = "abc";
+	string s2 = "abc"; //abs, abq, ab, a, qa, cba
+	cout << s2.compare(s1) << endl;
+	return 0;
+	*/
+	
+	/*
+	string s1 = "qwerty";
+	s1.clear();
+	if(!s1.empty()){
+		cout<<"String is not empty";
+	}
+	else{
+		cout<<"String is empty";
+	}
+	*/
+	
+	/*
+	string s1 = "GitHubcom";
+	cout<<s1.find("com");
+	*/
+	
+	/*
+	string s1 = "Hello ";
+	cout<<s1.insert(6, "World!");
+	*/
+	
+	/*
+	string s1 = "qwertyuio";
+	cout<<s1.size()<<" "<<sizeof(s1)/sizeof(s1[2])<<" "<<s1.length();
+	*/
+	
+	/*
+	string s1 = "qwertyuiop";
+	cout<<s1.substr(6, 5);
+	*/
+	
+	/*
+	string s1 = "123";
+	int n = stoi(s1);
+	cout<<n;
+	*/
+	
+	/*
+	int n = 123;
+	string s1 = to_string(n);
+	cout<<s1;
+	*/
+	
+	/*
+	string s1 = "qwertyuioplkjhgfdsazxcvbnm";
+	sort(s1.begin(), s1.end());
+	cout<<s1;
+	*/
+	
+	/*
+	string s1 = "abcdefghijklmnopqrstuvwxyz";
+	reverse(s1.begin(), s1.end());
+	cout<<s1;
+	*/
+}
