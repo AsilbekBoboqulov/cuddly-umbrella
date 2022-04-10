0fb965ff5e8fea468443bc77fde9adaf1fffbea7
AsilbekBoboqulov <93362474+AsilbekBoboqulov@users.noreply.github.com>
2022-04-10 21:06:30 +0500

    Create main_82.cpp
    elements, chars, adress

---
+++ main_82.cpp
@@ -0,0 +1,34 @@
+/*
+#include <iostream>
+using namespace std;
+int main(){
+	//int a = 12.9;
+	//printf("%d",a);
+	
+	//float a = 12.9;
+	//int b = static_cast<int>(a);
+	//cout<<b;
+}
+*/
+/*
+#include <iostream>
+using namespace std;
+int main()
+{
+	for(int i = 1; i < 128; i++){
+	char a{i};
+	cout<<a<<" ";
+	}	
+}
+*/
+/*
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a = 10;
+	int *b{&a};
+	cout<<b<<endl;
+	cout<<*b<<endl;
+}
+*/
