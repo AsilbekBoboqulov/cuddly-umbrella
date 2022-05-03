b14508ce4f98c85b22f44b219a234bdd414dfde4
AsilbekBoboqulov <93362474+AsilbekBoboqulov@users.noreply.github.com>
2022-05-03 18:16:34 +0500

    Create main_87.cpp
    pionter in cpp

---
+++ main_87.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char * str = "G";
+    printf("\n%s\n",str);
+    printf(str); cout<<endl;
+    char arr[] = "github.com";
+    arr[0] = 'G';
+    printf("%s\n",arr);
+    return 0;
+}
