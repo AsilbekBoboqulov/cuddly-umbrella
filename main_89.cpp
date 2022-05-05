45b4a5437db99b5694a88a221f46decb1823cd65
AsilbekBoboqulov <93362474+AsilbekBoboqulov@users.noreply.github.com>
2022-05-05 21:09:38 +0500

    Create main_89.cpp
    size of datatype

---
+++ main_89.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+int main()
+{
+    printf("char %d\n",sizeof(char));
+    printf("bool %d\n",sizeof(bool));
+    printf("short int %d\n",sizeof(short int));
+    printf("int %d\n",sizeof(int));
+    printf("signed int %d\n",sizeof(signed int));
+    printf("float %d\n",sizeof(float));
+    printf("long long %d\n",sizeof(long long));
+    printf("double %d\n",sizeof(double));
+    printf("unsigned long long int %d\n",sizeof(unsigned long long int));
+}
