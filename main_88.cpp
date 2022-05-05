8a4bcc2686824539ade07552392e89d09b2e3e08
AsilbekBoboqulov <93362474+AsilbekBoboqulov@users.noreply.github.com>
2022-05-05 14:38:41 +0500

    Create main_88.cpp
    more codes

---
+++ main_88.cpp
@@ -0,0 +1,39 @@
+/*
+#include <iostream>
+using namespace std;
+const int a = 10;
+int main()
+{
+    cout << a << endl;
+    int a = 2;
+    cout << a << endl;
+    int b = a;
+    cout << b << endl;
+    int d = ::a;
+    cout << d;
+    return 0;
+}
+*/
+
+/*
+#include <iostream>
+using namespace std;
+int main()
+{
+    bool a = {true}; //a === 1 === {R: Num} == string
+    cout << a << endl;
+    bool b = {false}; //b === 0 === not a
+    cout << b << endl;
+    return 0;
+}
+*/
+
+/*
+#include <iostream>
+using namespace std;
+int main()
+{
+	 bool a = true, b = false;
+	 bool d = (a == b) ^ a;  cout << d; //d = (1 == 0) ^ 1 = 0 ^ 1 = 1;
+}
+*/
