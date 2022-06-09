dc909c33a1b71029664c4a82cd8f4094f6619ba1
AsilbekBoboqulov <93362474+AsilbekBoboqulov@users.noreply.github.com>
2022-06-09 23:56:34 +0500

    Create main_92.cpp
    super shorter

---
+++ main_92.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    typedef long long ll;
+    typedef unsigned long long ull;
+    ll n = pow(2,31)-1;
+    ull m = pow(2,63)-1;
+    cout<< n << "\n" << m;
+}
