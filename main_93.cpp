19f9ccc86f9f9efb157efa44aee381e6f4536994
AsilbekBoboqulov <93362474+AsilbekBoboqulov@users.noreply.github.com>
2022-06-10 23:52:32 +0500

    Create main_93.cpp
    working with a file

---
+++ main_93.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	typedef long long ll;
+	ll n, m;
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	cin >> n >> m;
+	cout << n + m;
+}
