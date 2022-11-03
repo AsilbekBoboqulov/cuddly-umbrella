17b703241b3efa6b5aa4c4ca44cc39f37060bcc9
AsilbekBoboqulov <93362474+AsilbekBoboqulov@users.noreply.github.com>
2022-11-04 00:23:58 +0500

    Create main_111.cpp
    remove n-th element of vector

---
+++ main_111.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define int ll
+#define all(x) (x).begin(),(x).end()
+const int mod = 1e9+7;
+const double PI = acos( -1. );
+void in(){
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+}
+void solve(){
+    //in();
+    vector<int> vec(5);
+    iota(all(vec), 1);
+    for(auto x:vec) cout << x << ' '; cout << '\n';
+    vec.erase(vec.begin() + 2); // 2 or n th element
+    for(auto x:vec) cout << x << ' ';
+}
+signed main(){
+    cin.tie(0)->sync_with_stdio(0);
+    int t; t = 1;
+    //cin >> t;
+    while(t--) solve();
+}
+/*
+        ██╗  ██╗███╗   ██╗██╗   ██╗ █████╗ ███████╗        
+        ██║ ██╔╝████╗  ██║╚██╗ ██╔╝██╔══██╗╚══███╔╝        
+        █████╔╝ ██╔██╗ ██║ ╚████╔╝ ███████║  ███╔╝         
+        ██╔═██╗ ██║╚██╗██║  ╚██╔╝  ██╔══██║ ███╔╝          
+███████╗██║  ██╗██║ ╚████║   ██║   ██║  ██║███████╗███████╗
+╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝
+*/
