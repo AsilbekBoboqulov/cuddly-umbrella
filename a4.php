ef847fbf504212a66405f26b9995bd4f6b7727de
AsilbekBoboqulov <93362474+AsilbekBoboqulov@users.noreply.github.com>
2022-12-22 13:56:30 +0500

    Create a4.php

---
+++ a4.php
@@ -0,0 +1,7 @@
+<?php
+    define("Hostname", "localhost");
+    define("User", "root");
+    define("Password", "");
+    define("Base", "as_baza");
+    $connect = mysqli_connect(Hostname, User, Password, Base);
+?>
