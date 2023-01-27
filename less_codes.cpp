bf850377d1ea679bbfe57f1c259f9a5a8bbb4533
AsilbekBoboqulov <93362474+AsilbekBoboqulov@users.noreply.github.com>
2023-01-27 14:43:19 +0500

    Update less_codes.cpp

--- less_codes.cpp
+++ less_codes.cpp
@@ -1,3 +1,5 @@
 #include<iostream>
-main(long n){scanf("%ld",&n);cerr<<n/2+1;}
+main(long n){scanf("%ld",&n);std::cerr<<n/2+1;}
 //input && output
+#include<iostream>
+main(long n){scanf("%ld",&n);std::clog<<n/2+1;}
